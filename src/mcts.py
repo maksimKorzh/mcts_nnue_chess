99095376e32cac0cae42edbc835748a61d6052b4
Maksim Korzh <freesoft.for.people@gmail.com>
2020-11-15 13:59:49 +0200

    final commit

--- src/mcts.py
+++ src/mcts.py
@@ -4,6 +4,7 @@ from ctypes import *
 import time
 import math
 import random
+import chess
 
 # load NNUE shared library
 nnue = cdll.LoadLibrary("./libnnueprobe.so")
@@ -31,11 +32,25 @@ def nnue_policy(state):
     # case draw
     elif state.board.can_claim_draw(): return 0
         
-    # case in insufficient material  
+    # case in insufficient material
     elif state.board.is_insufficient_material(): return 0
     
     # get NNUE evaluation score
-    return nnue.nnue_evaluate_fen(bytes(state.board.fen(), encoding='utf-8'))
+    score = nnue.nnue_evaluate_fen(bytes(state.board.fen(), encoding='utf-8'))
+    
+    # on material inbalance
+    if get_material_score(state):
+        # get quiescence score
+        quiescence_score = quiescence(state, -10000, 10000)
+        
+        # use either direct static evaluation score or quiescence score
+        # depending on which on is greater
+        return max(score, quiescence_score)
+    
+    # on material balance
+    else:
+        # use static evaluation score
+        return score
 
 class treeNode():
     def __init__(self, state, parent):
@@ -47,6 +62,70 @@ class treeNode():
         self.totalReward = 0
         self.children = {}
 
+def get_material_score(state):
+    # material score
+    score = 0
+
+    # relative piece values
+    material = {
+        'P': 100,
+        'N': 300,
+        'B': 350,
+        'R': 500,
+        'Q': 900,
+        'K': 1000,
+        'p': -100,
+        'n': -300,
+        'b': -350,
+        'r': -500,
+        'q': -900,
+        'k': -1000,
+    }
+    
+    # loop over board squares
+    for square in range(64):
+        # init piece
+        piece = state.board.piece_at(square)
+        
+        # if not empty square
+        if piece is not None:
+            # calculate material score
+            score += material[str(piece)]
+    
+    # return material score
+    return score
+
+# quiescence search
+def quiescence(state, alpha, beta):
+    # static evaluation score
+    stand_pat = nnue.nnue_evaluate_fen(bytes(state.board.fen(), encoding='utf-8'))
+    
+    if stand_pat >= beta:
+        return beta;
+    
+    if alpha < stand_pat:
+        alpha = stand_pat
+
+    # loop over legal moves
+    for move in state.board.legal_moves:
+        # pick up only captures
+        if state.board.is_capture(move):
+            # make move on board
+            state.board.push(chess.Move.from_uci(str(move)))
+            
+            # recursive quiescence call
+            score = -quiescence(state, -beta, -alpha)
+            
+            # take move back (restore board position)
+            state.board.pop()
+
+            if score >= beta:
+                return beta
+
+            if score > alpha:
+               alpha = score
+    
+    return alpha;
 
 class mcts():
     def __init__(self, timeLimit=None, iterationLimit=None, explorationConstant=1 / math.sqrt(2),
@@ -80,7 +159,7 @@ class mcts():
                 self.executeRound()
 
         bestChild = self.getBestChild(self.root, 0)
-        return self.getAction(self.root, bestChild), nnue_policy(bestChild.state)
+        return self.getAction(self.root, bestChild), -nnue_policy(bestChild.state)
 
     def executeRound(self):
         node = self.selectNode(self.root)
@@ -133,3 +212,9 @@ class mcts():
         for action, node in root.children.items():
             if node is bestChild:
                 return action
+
+
+
+
+
+
